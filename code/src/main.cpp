6651df66b5063b3d9b8fa15d4dd1065d1365efac
Dena-lab <denis.majrich@gmail.com>
2024-08-19 23:05:28 +0200

    Testing finished

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -2,27 +2,28 @@
 
 #include <FastLED.h>
 
-#define NUM_LEDS 100
+#define NUM_LEDS 300
 #define DATA_PIN 3
 
 CRGB leds[NUM_LEDS];
 
 void setup() { 
 
-    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(20);
 
 }
 
 void loop() { 
   fill_solid(leds, NUM_LEDS, CRGB::Red);
   FastLED.show();
-  delay(500);
+  delay(200);
   fill_solid(leds, NUM_LEDS, CRGB::Green);
   FastLED.show();
-  delay(500);
+  delay(200);
   fill_solid(leds, NUM_LEDS, CRGB::Blue);
   FastLED.show();
-  delay(500);
+  delay(200);
   fill_solid(leds, NUM_LEDS, CRGB::Black);
   FastLED.show();
   delay(500);
